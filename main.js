3052ec5508f19d168d9aa0605a08adb444b7d418
Tega kofi <kofitega@gmail.com>
2019-10-26 23:55:03 +0200

    todo list v2

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-function todo(){
-    var form = document.getElementById("FormInput").value;
+//adding form value to list
+ class form{
+    static addForm(){
+        var form = document.getElementById("FormInput").value;
     let uol = document.getElementById('uil');
     let liu = document.createElement("li");
-    liu.innerHTML = form;
-    console.log(form);
+      liu.innerHTML = `<p>${form} &nbsp <button class="btn btn-danger btn-sm del">X</button></p><hr>`;
+    console.log(liu);
     uol.appendChild(liu);
+    document.getElementById("FormInput").value = " "
+    }
+    //deleteabook
+    static deleteEntry(el){
+        if(el.classList.contains('del')){
+            console.log(el.classList);
+            el.parentElement.parentElement.remove();
+        }
+    }
     
-   
-
 }
+// Event add book
 let bit = document.getElementById("nutton");
-bit.addEventListener('click', todo, false);
+bit.addEventListener('click', form.addForm, false);
+
+//Remove book
+document.querySelector('#uil').addEventListener('click',(e)=>{
+    form.deleteEntry(e.target)});
